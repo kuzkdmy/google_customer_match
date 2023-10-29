76c38053055931e8992e1a11eff4026dea1272d3
Dmytro Kuzkin <dkuzkin@demandbase.com>
2023-10-29 22:59:13 +0200

    format

--- src/main/scala/com/test/gcm/service/OAuthService.scala
+++ src/main/scala/com/test/gcm/service/OAuthService.scala
@@ -1,9 +1,6 @@
 package com.test.gcm.service
 
-import com.google.api.client.googleapis.auth.oauth2.{
-  GoogleAuthorizationCodeFlow,
-  GoogleTokenResponse
-}
+import com.google.api.client.googleapis.auth.oauth2.{GoogleAuthorizationCodeFlow, GoogleTokenResponse}
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.gson.GsonFactory
 import com.google.api.client.util.store.MemoryDataStoreFactory
@@ -13,9 +10,8 @@ import zio.{Task, URLayer, ZIO, ZLayer}
 
 import java.util.Collections
 
-/**
- * https://developers.google.com/google-ads/api/docs/oauth/cloud-project
- */
+/** https://developers.google.com/google-ads/api/docs/oauth/cloud-project
+  */
 trait OAuthService {
   def oauthAttempt(): Task[OAuthAttempt]
   def completeOauthAttempt(id: String, code: String): Task[GoogleTokenResponse]
@@ -26,18 +22,18 @@ case class OAuthServiceImpl(conf: GoogleServerApiCredsConfig) extends OAuthServi
 
   override def oauthAttempt(): Task[OAuthAttempt] = {
     for {
-      id <- zio.Random.nextUUID.map(_.toString)
-      _ <- ZIO.logInfo(s"init oauth attempt, id:$id")
-      auth <- authCodeFlow()
+      id          <- zio.Random.nextUUID.map(_.toString)
+      _           <- ZIO.logInfo(s"init oauth attempt, id:$id")
+      auth        <- authCodeFlow()
       redirectUrl <- ZIO.attempt(auth.newAuthorizationUrl().setRedirectUri(redirectUri).build())
-      _ <- ZIO.logInfo(s"init oauth attempt, id:$id success, redirect url: $redirectUrl")
+      _           <- ZIO.logInfo(s"init oauth attempt, id:$id success, redirect url: $redirectUrl")
     } yield OAuthAttempt(id, redirectUrl)
   }
 
   override def completeOauthAttempt(id: String, code: String): Task[GoogleTokenResponse] = {
     for {
-      _ <- ZIO.logInfo("complete oauth attempt")
-      auth <- authCodeFlow()
+      _         <- ZIO.logInfo("complete oauth attempt")
+      auth      <- authCodeFlow()
       tokenResp <- ZIO.attempt(auth.newTokenRequest(code).setRedirectUri(redirectUri).execute())
       _ <- ZIO.logInfo(s"""complete oauth success,
            |token expire in seconds: ${tokenResp.getExpiresInSeconds},
@@ -54,10 +50,12 @@ case class OAuthServiceImpl(conf: GoogleServerApiCredsConfig) extends OAuthServi
       GsonFactory.getDefaultInstance,
       conf.clientId,
       conf.clientSecret,
-      Collections.singletonList("https://www.googleapis.com/auth/adwords"))
+      Collections.singletonList("https://www.googleapis.com/auth/adwords")
+    )
       .setAccessType("offline")
       .setDataStoreFactory(new MemoryDataStoreFactory())
-      .build())
+      .build()
+  )
 
 }
 
